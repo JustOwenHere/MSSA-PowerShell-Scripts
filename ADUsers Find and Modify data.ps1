7f8e61d61fbef4a6b8516e544fe51ccb05262315
Owen Graham <owen.j.graham@outlook.com>
2021-12-01 22:08:10 -0800

    Two example scripts that will find and modify specific active directory user data.

---
+++ ADUsers Find and Modify data.ps1
@@ -0,0 +1,11 @@
+﻿#Display name, city and department in alphabetical order of all users located in London.
+Get-ADUser -Filter * -Properties City,Department |
+Where-Object {$_.City -eq 'London' -and $_.Department -eq 'IT'} |
+Sort-Object -Property Name |
+Select-Object -Property Name,City,Department
+
+
+#Set all users under the above filter to be in located in office LON-A/1000.
+Get-ADUser -Filter * -Properties City,Department |
+Where-Object {$_.City -eq 'London' -and $_.Department -eq 'IT'} |
+Set-ADUser -Office "LON-A/1000"
