a64e400363bc218a62df02f1ce0e7366fb2c7c6e
Owen Graham <owen.j.graham@outlook.com>
2021-12-05 14:44:07 -0800

    Powershell example script for generating an ADUser report and exporting it to an HTML file with some pretty CSS.

---
+++ ADUsers HTML Export w CSS.ps1
@@ -0,0 +1,20 @@
+﻿$CSS = @'
+<style>
+
+table {border:1pt solid black;width:100%;border-collapse:collapse;}
+
+tr,td {border:1pt solid black;border-collapse:collapse;}
+
+th {border:1pt solid black;background-color:green;border-collapse:collapse;}
+
+</style>
+'@
+
+
+
+Get-ADUser -Filter * -Properties Department,City,Office | 
+Where-Object {$_.Department -eq ‘IT’ -and $_.City -eq ‘London’} | 
+Select-Object -Property Name,Department,City,Office | 
+Sort-Object -Property Name |
+ConvertTo-Html -PreContent 'Users' -Head $CSS |
+Out-File E:\UserReport.html
